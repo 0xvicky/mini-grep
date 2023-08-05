c6ca65806d5e2db25c63ba294e1e3f0a95e20906
vicky <tyagivivek528@gmail.com>
2023-08-05 15:44:16 +0530

    Input file content reading started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("CLI APP");
+    println!("Mini-Grep"); //Search string in a given file
+
+    let args: Vec<String> = env::args().collect();
+    println!("Query: {:?}", &args[1]);
+    println!("Filename: {:?}", &args[2]);
+
+    let content =
+        fs::read_to_string(&args[2]).expect("Something went wrong while reading the file");
+    println!("The File Content:{:?}", content);
 }
