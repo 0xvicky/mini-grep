9a5798d24e46c1f982b303dde10911fb85fa7cec
vicky <tyagivivek528@gmail.com>
2023-08-06 19:08:01 +0530

    Port all the functions into lib.rs and run 1 test for search functionality

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content: String = fs::read_to_string(config.filename)?;
+    println!("The File Content:{}", content);
+    Ok(())
+}
+
+pub struct Config {
+    //To relate both the query and the filename
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Insufficient Arguments");
+        }
+        let query: &String = &args[1];
+        let filename: &String = &args[2];
+        Ok(Config {
+            query: query.clone(),
+            filename: filename.clone(),
+        })
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+
+    fn search_test() {
+        let query = "fuct";
+        let content = "/
+        Pick three:
+        Safe, Fast, Profuctive
+        Exit
+        ";
+
+        assert_eq!(vec!["Safe, Fast, Profuctive"], search(query, content));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
+use cli_app::Config;
 use std::env;
-use std::fs;
 use std::process;
-
 fn main() {
     println!("Mini-Grep"); //Search string in a given file
 
     let args: Vec<String> = env::args().collect();
-    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+    let config: Config = Config::new(&args).unwrap_or_else(|err: &str| {
         println!("Error occured:{}", err);
         process::exit(1);
     });
 
     println!("Query: {:?}", config.query);
     // println!("Filename: {:?}", &args[2]);
-    let content =
-        fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
-    println!("The File Content:{:?}", content);
-}
-
-struct Config {
-    //To relate both the query and the filename
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Insufficient Arguments");
-        }
-        let query = &args[1];
-        let filename = &args[2];
-        Ok(Config {
-            query: query.clone(),
-            filename: filename.clone(),
-        })
+    if let Err(e) = cli_app::run(config) {
+        println!("Application Error:{}", e);
+        process::exit(1);
     }
 }
